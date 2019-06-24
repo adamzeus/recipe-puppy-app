4ccc238359fdd283b706c6e391004c84f1f19a2a
adamzeus <adamdyeruk@hotmail.com>
2019-06-24 20:43:30 +0100

    Fix wrong name

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  endpointURL: 'http://www.recipepuppy.com',
-  corsHelperURL: 'https://cors-anywhere.herokuapp.com'
+  RecipePuppyURL: 'http://www.recipepuppy.com',
+  corsHelpURL: 'https://cors-anywhere.herokuapp.com'
 };
